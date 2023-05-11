44d709de04757d1bba500beb6a687db1dcbd47ef
Jayshri <charpe.jayshri@gmail.com>
2023-05-11 15:07:48 +0530

    useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 // import Restaurant from './components/Restaurant'
 // import UseState from './components/Hooks/UseState.js'
-import UseEffect from './components/Hooks/useEffect'
+// import UseEffect from './components/Hooks/useEffect'
+import UseReducer from './components/Hooks/useReducer'
 
 const App = () => {
   return (
     <div>
       {/* <Restaurant/> */}
       {/* <UseState/> */}
-      <UseEffect />
+      {/* <UseEffect /> */}
+      <UseReducer />
     </div>
   )
 }
